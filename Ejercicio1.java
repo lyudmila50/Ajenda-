68716b475dc20673b66bd5d2a0cf0a6da9ec3ed4
lyudmila50 <81586606+lyudmila50@users.noreply.github.com>
2022-04-05 13:37:14 -0300

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class Ejercicio1 {
+    public static void main(String[] args) {
+         
+        System.out.println("Agenda de " + nombre);
+        
+
+        Scanner sca = new Scanner(System.in);
+        final String nombre  = sca.nextLine();
+        System.out.println("nombre:"+ nombre);
+       
+        while(opc!=3){
+        System.out.println("----------***************---------------");
+        System.out.println("               MENU                       ");
+        System.out.println(" 1- Ingrese un nuevo contacto");
+        System.out.println(" 2- Consutar un conacto existe");
+        System.out.println("3-Salir ");
+        System.out.println("---------------------------------------------");
+        System.out.println("ingrese una opcion");
+        int opc =sca.nextInt();
+
+        switch(opi){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+               break;
+               default:System.out.println("su opcion no es correcta");
+        }
+
+    
+       
+       
+       
+
+
+
+    }
+}
+}
